f9da834e3e9aa37e9b7c71d8ef57c12e9b7fb260
1OpoH1 <75303679+1OpoH1@users.noreply.github.com>
2023-03-10 02:10:31 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-def make_regex_string(array: list):
-        try:
-            array = '|'.join(array)
-        except TypeError:
-            for i in range(len(array)):
-                array[i] = make_string(array[i])
-        if type(array) == list:
-            return '|'.join(array)
-        else:
-            return array
+import time
+import hashlib
+import requests
+z = f'{round(time.time())}oeh2VDE78zNKAiFNXrKz6VU3'
+hash_object = hashlib.sha1(z.encode())
+print(z)
+hash = hash_object.hexdigest()[5:16]
+print(hash)
+params = {'time': round(time.time()), 'hash': hash}
+r = requests.get("https://nebo.live/api/v2/cities/krs", params=params, headers={'X-Auth-Nebo': 'z1hpY1bkCTaiMhMik7cEAJqq'})
+print(r.json())
+print(round(time.time()))
 
-
-
+for i in r.json():
+    print(i['name'])
